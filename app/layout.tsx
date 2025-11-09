7514e6e0801e777e4a735f5d1246569a82bf1ffc
talocacoes-maker <t.alocacoes@talocacoes.com>
2025-11-09 20:21:18 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Menu from "@/components/Menu"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Menu />
+        <main className="ml-64 p-6 bg-gray-100 min-h-screen">{children}</main>
+      </body>
     </html>
   )
 }
